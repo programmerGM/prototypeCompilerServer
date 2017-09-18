6f7809ed3c2746b0b269818cbbba7d7cc1c74e65
luandr <stringigualanull@outlook.com>
2017-09-18 16:05:44 -0300

    Commit 2

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -23,6 +23,8 @@ module.exports.validar = (application, req, res) => {
   var validacao = {}, tokens = [], posicao = 0;
   var literal = false, literalOpen = 0;
   var number = false, numberOpen = 0, numAcumula = "";
+  var wordAux = "", firstLetter = false;
+  //var hasNext = false, hasNextNext=false;
   // remove espaço em branco
   // @deprecated text = text.replace(/(\r\n|\n|\r)/gm, "");
   // text = text.replace(/\s/g, '');
@@ -30,7 +32,7 @@ module.exports.validar = (application, req, res) => {
   var lines = text.split('\n');
 
   for (var a = 0; a < lines.length; a++) {
-    lines[a] = lines[a].replace(/\s/g, '');
+    //lines[a] = lines[a].replace(/\s/g, '');
     textaux = lines[a];
     for (var b = 0; b < textaux.length; b++) {
       var l = textaux[b];
@@ -57,6 +59,129 @@ module.exports.validar = (application, req, res) => {
       if (literal && !(l.match(/[\"]+/g))) {
         continue;
       }
+
+      // verifica alguns single character
+      switch (true) {
+        // } - 35
+        case /[}]+/g.test(l):
+          tokens.push(new Token(a, l, null, 35));
+          continue;
+        // { - 36
+        case /[{]+/g.test(l):
+          tokens.push(new Token(a, l, null, 36));
+          continue;
+        // ; - 37
+        case /[;]+/g.test(l):
+          tokens.push(new Token(a, l, null, 37));
+          continue;
+        // : - 38
+        case /[:]+/g.test(l):
+          tokens.push(new Token(a, l, null, 38));
+          continue;
+        // , - 40
+        case /[,]+/g.test(l):
+          tokens.push(new Token(a, l, null, 40));
+          continue;
+        // ) - 42
+        case /[)]+/g.test(l):
+          tokens.push(new Token(a, l, null, 42));
+          continue;
+        // , - 43
+        case /[(]+/g.test(l):
+          tokens.push(new Token(a, l, null, 43));
+          continue;
+
+      }
+
+
+
+
+
+      if (l.match(/[a-z]+/g)) {
+        // armazena a letra
+        wordAux += l;
+
+        /* se não tiver lendo uma sequencia de de letras
+         então seta para true e reseta o auxiliar */
+        if (!firstLetter) {
+          firstLetter = !firstLetter;
+        }
+        // se o proximo não for uma letra então pode finalizar
+        if (!(next.match(/[a-z]+/g))) {
+          firstLetter = !firstLetter;
+          console.log(wordAux);
+          temp = wordAux;
+          wordAux = "";
+
+          //verifica qual palavra é
+          switch (true) {
+            // while - 1
+            case /while+/g.test(temp):
+              tokens.push(new Token(a, "while", null, 1));
+              continue;
+            // void - 2
+            case /void+/g.test(temp):
+              tokens.push(new Token(a, "void", null, 2));
+              continue;
+            // string - 3
+            case /string+/g.test(temp):
+              tokens.push(new Token(a, "string", null, 3));
+              continue;
+            // return - 4
+            case /return+/g.test(temp):
+              tokens.push(new Token(a, "return", null, 4));
+              continue;
+            // main - 10
+            case /main+/g.test(temp):
+              tokens.push(new Token(a, "while", null, 10));
+              continue;
+            // inicio - 13
+            case /inicio+/g.test(temp):
+              tokens.push(new Token(a, "inicio", null, 13));
+              continue;
+            // if - 14
+            case /if+/g.test(temp):
+              tokens.push(new Token(a, "if", null, 14));
+              continue;
+            // for - 16
+            case /for+/g.test(temp):
+              tokens.push(new Token(a, "for", null, 16));
+              continue;
+            // fim - 18
+            case /fim+/g.test(temp):
+              tokens.push(new Token(a, "fim", null, 18));
+              continue;
+            // else - 19
+            case /else+/g.test(temp):
+              tokens.push(new Token(a, "else", null, 19));
+              continue;
+            // do - 20
+            case /do+/g.test(temp):
+              tokens.push(new Token(a, "do", null, 20));
+              continue;
+            // cout - 21
+            case /cout+/g.test(temp):
+              tokens.push(new Token(a, "cout", null, 21));
+              continue;
+            // cin - 22
+            case /cin+/g.test(temp):
+              tokens.push(new Token(a, "cin", null, 22));
+              continue;
+            // callfuncao - 24
+            case /callfuncao+/g.test(temp):
+              tokens.push(new Token(a, "callfuncao", null, 24));
+              continue;
+
+          }
+
+
+
+
+        }
+      }
+
+
+
 
       // verifica char
       if (l.match(/[']+/g)) {
@@ -134,15 +259,13 @@ module.exports.validar = (application, req, res) => {
       // se é operador de comparação
       if (l.match(/[<>!=]+/g)) {
         if (l.match(/[<]+/g)) {
-          // << cin
+          // << cin -  31
           if (next.match(/[<]+/g)) {
-            tok = "<<";
-            tokens.push(new Token(a, tok, null, 0));
+            tokens.push(new Token(a, "<<", null, 31));
           } else {
-            // <= menor ou igual que
+            // <= menor ou igual que - 30
             if (next.match(/[=]+/g)) {
-              tok = "<=";
-              tokens.push(new Token(a, tok, null, 0));
+              tokens.push(new Token(a, "<=", null, 30));
             }
           }
 
@@ -150,53 +273,42 @@ module.exports.validar = (application, req, res) => {
         } else {
 
           if (l.match(/[>]+/g)) {
-            // >> cin
+            // >> cin - 25
             if (next.match(/[>]+/g)) {
-              tok = ">>";
-              tokens.push(new Token(a, tok, OPERADOR_IO_MSG, false));
-
+              tokens.push(new Token(a, ">>", null, 25));
+              b += 1;
             } else {
-              // <= maior ou igual que
+              // >= maior ou igual que 26
               if (next.match(/[=]+/g)) {
-                tok = ">=";
-                tokens.push(new Token(a, tok, OPERADOR_LOG_MSG, false));
-              } else {
-                tok = "=";
-                msg = "Esperava \">=\" entrava inválida";
-                tokens.push(new Token(a, tok, msg, true));
-                continue;
+                tokens.push(new Token(a, ">=", null, 26));
+                b += 1;
               }
+
             }
-            b += 1;
           } else {
 
-            // != diferente
+            // != diferente - 45
             if (l.match(/[!]+/g)) {
               if (next.match(/[=]+/g)) {
-                tok = "!=";
-                tokens.push(new Token(a, tok, OPERADOR_LOG_MSG, false));
+                tokens.push(new Token(a, "!=", null, 45));
+                b += 1;
               } else {
-                tok = "!";
                 msg = "Esperava \"!=\" entrava inválida";
-                tokens.push(new Token(a, tok, msg, true));
+                tokens.push(new Token(a, null, msg, null));
                 continue;
               }
-
-              b += 1;
             } else {
               // == igual
               if (l.match(/[=]+/g)) {
-
+                // == - 28
                 if (next.match(/[=]+/g)) {
-                  tok = "==";
-                  tokens.push(new Token(a, tok, null, 0));
-                } else {
-                  msg = "Esperava \"==\" entrava inválida";
-                  tokens.push(new Token(a, null, msg, null));
+                  tokens.push(new Token(a, "==", null, 28));
+                  b += 1;
                   continue;
                 }
-
-                b += 1;
+                // se for = - 29
+                tokens.push(new Token(a, "=", null, 29));
+                continue;
 
               }
             }
@@ -224,6 +336,9 @@ module.exports.validar = (application, req, res) => {
   if (literal) {
     tokens.push(new Token(literalOpen, null, LITERAL_CLOSE_MSG, null));
   }
+
+  // fim de arquivo $ - 44
+  tokens.push(new Token(a, "$", null, 44));
 
   console.log(tokens);
 
